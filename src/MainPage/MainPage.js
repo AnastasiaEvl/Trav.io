4ee1b1bc5977eeacd03272b235a6d0bf0e6878f4
AnastasiaEvl <nastya.e.1234@gmail.com>
2021-10-20 12:09:26 +0300

    second block added to mainPage

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../MainPage/MainPage.css";
 import FirstBlock from "./FirstBlock";
+import SecondBlock from "./SecondBlock";
 
 function MainPage() {
   return (
     <div className="main">
       <FirstBlock />
+      <SecondBlock />
     </div>
   );
 }
