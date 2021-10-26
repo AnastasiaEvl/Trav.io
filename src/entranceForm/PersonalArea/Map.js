398eaa20cf232e1bff8bf6c31dbd00c9f6500a03
AnastasiaEvl <nastya.e.1234@gmail.com>
2021-10-27 00:36:33 +0300

    Work map

--- src/entranceForm/PersonalArea/Map.js
+++ src/entranceForm/PersonalArea/Map.js
@@ -1,49 +1,57 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./PersonalAreaStyle.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
+
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiYW5hc3Rhc2lhZXZsIiwiYSI6ImNrdjVidTg5aDBrNTcycW9rZjJrYjZyYXYifQ.cc8dzHKC47e0M31mSwAi9g";
+    "pk.eyJ1IjoiYW5hc3Rhc2lhZXZsIiwiYSI6ImNrdjVidTg5aDBrNTcycW9rZjJrYjZyYXYifQ.cc8dzHKC47e0M31mSwAi9g";
 
 let map;
-var geocoder = new MapboxGeocoder();
+let geocoder;
 
 function Map() {
-  const mapContainer = useRef(null);
 
-  useEffect(() => {
-    map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [27, 53],
-      zoom: 6,
+    const [address, setAddress] = useState();
+    const [coord, setCoord] = useState();
+    const mapContainer = useRef(null);
+
+    useEffect(() => {
+        map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [27, 53],
+            zoom: 6,
+        });
+        geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl,
+        });
+        map.addControl(
+            geocoder,
+            "top-left"
+        );
+
+        geocoder.on('result', (results) => (
+            setAddress(results.result.place_name),
+                setCoord(results.result.center)
+        ))
     });
-    map.addControl(
-      new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl,
-      }),
-      "top-left"
+
+
+    return (
+        <div>
+            <div
+                id="map"
+                ref={mapContainer}
+                className="map-container"
+                style={{width: "100%", height: "80hv", marginLeft: "400px"}}
+            />
+            <div id="geocoder" class="geocoder"/>
+        </div>
     );
-  });
-
-  geocoder.on("results", function (results) {
-    console.log(results);
-  });
-
-  return (
-    <div>
-      <div
-        id="map"
-        ref={mapContainer}
-        className="map-container"
-        style={{ width: "100%", height: "80hv", marginLeft: "400px" }}
-      />
-      <div id="geocoder" class="geocoder" />
-    </div>
-  );
 }
+
 export default Map;
